ea27e56c337bb4fa9dcb1893376c4e91873674e0
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-05-05 13:41:00 +0200

    feat: add images entity with enum ImageSourcePosibility

---
+++ NGS/Domain/Entities/Images.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Consts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Images
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public byte[]? Image { get; set; }
+        public string? UploadName { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeleteDate { get; set; }
+        public DateTime AddDate { get; set; }
+        public ImageSourcePossibility ImageSource { get; set; }
+
+        public Images()
+        {
+            IsDeleted = false;
+            AddDate = DateTime.Now;
+        }
+
+        public Images(byte[] image, ImageSourcePossibility imageSource):this()
+        {
+            Image = image;
+            ImageSource = imageSource;
+        }
+    }
+}
---
+++ NGS/Domain/Migrations/20230505112940_ImagesInit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImagesInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    UploadName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeleteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AddDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageSource = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
