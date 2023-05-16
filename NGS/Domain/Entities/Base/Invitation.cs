f34bb819a7594412edca2a0ed4094f58ab1ef6be
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-05-16 21:04:52 +0200

    feat: add base invintation type

---
+++ NGS/Domain/Entities/Base/Invitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    public class Invitation
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public User UserInvited { get; set; }
+        public User CreatorOfInvite { get; set; }
+        public bool? Accepted { get; set; }
+        public bool? PreventFromNextInvite { get; set; }
+    }
+}
