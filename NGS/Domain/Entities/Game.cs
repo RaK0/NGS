91efd230a1ae7dc128d257c1f859dadf14672da3
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-05-16 12:11:49 +0200

    feat: add Games enums for static type of game and Game entity for description full name

---
+++ NGS/Domain/Entities/Game.cs
@@ -0,0 +1,33 @@
+﻿using NGS.Shared.Consts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Game
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string GameName { get; set; }
+        public string Description { get; set; }
+        public Games GameType { get; set; }
+        public DateTime CreateTime { get; set; }
+        public bool IsDisabled { get; set; }
+        
+        public Game()
+        {
+            CreateTime = DateTime.Now;
+            IsDisabled = false;
+        }
+        public Game(string gameName, string description, Games gameType) : this()
+        {
+            GameName = gameName;
+            Description = description;
+            GameType = gameType;
+        }
+    }
+}
---
+++ NGS/Shared/Consts/SharedConsts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGS.Shared.Consts
+{
+    public enum Games
+    {
+        LeagueOfLegend,
+        Metin2,
+        CSGo,
+        CSSource
+    }
+}
