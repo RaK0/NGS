d13dfba6a27230d0944e6cc71b21f5d1bebd67d1
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-05-16 21:11:41 +0200

    feat: add GameTeamInv entity

---
+++ NGS/Domain/Entities/GameTeamInvitation.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class GameTeamInvitation : Invitation
+    {
+        public GameTeam Team { get; set; }
+        public GameTeamInvitation()
+        {
+            DateSend = DateTime.Now;
+        }
+
+        public GameTeamInvitation(User inviting, User invited, GameTeam team) : this()
+        {
+            CreatorOfInvite = inviting;
+            UserInvited = invited;
+            Team = team;
+        }
+    }
+}
