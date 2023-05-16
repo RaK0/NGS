ac16fae8b16f350a4ee8fc7a9749776e4f17cd7b
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-05-16 20:50:15 +0200

    feat: add collection for game, some stuff can use multiple games or single

--- NGS/Domain/Entities/Base/GameStuff.cs
+++ NGS/Domain/Entities/Base/GameStuff.cs
@@ -14,7 +14,8 @@ namespace Domain.Entities.Base
         public string Name { get; set; }
         public string Description { get; set; }
         public Image Image { get; set; }
-        public Game Game { get; set; }
+        public virtual ICollection<Game> Games { get; set; }
+        public virtual Game Game { get; set; }
         public virtual bool IsConfirm { get; set; }
     }
 }
